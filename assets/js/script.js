e24e8ef156a1f0bcfba1dffb7851971d6d9aa86f
Tunde <Foltunde@gmail.com>
2024-03-27 11:10:25 +0000

    create the eventlisterner create the click button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
+// wait for the addEventlisterner to the js 
+
+document.addEventListener("DOMContentLoaded",function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of  buttons){ 
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") == "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert('You clicked $(gameType)');
+            }
+        })
+    }
+})
 function runGame () {
+
 }
 function checkAnswer(){
 
@@ -25,5 +42,5 @@ function displaySubtractQuestion () {
 }
 
 function displayMultipleQuestion() { 
-    
+
 }

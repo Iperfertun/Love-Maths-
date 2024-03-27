e628a2485035f3264b21679067325d5a047d3809
Tunde <Foltunde@gmail.com>
2024-03-27 10:43:46 +0000

    create the fucntion for many variables

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function runGame () {
+}
+function checkAnswer(){
+
+}
+
+function calculateAnswer(){
+
+}
+ 
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer (){ 
+
+}
+
+function displayAdditionQuestion (){
+
+}
+ 
+function displaySubtractQuestion () {
+                                                                                                                                                                                                  
+}
+
+function displayMultipleQuestion() { 
+    
+}
